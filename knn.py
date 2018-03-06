5409b7f84988f313e561378a25d9fd29b6b75171
Gary Yim <gary.k.yim@gmail.com>
2018-03-05 19:50:58 -0500

    Removed itemgetter from Knn method and updated test.

--- knn.py
+++ knn.py
@@ -11,4 +11,11 @@ def classify(new_xs, factors, labels, k):
         ith_label = labels[distance_ranks[i]]
         tally[ith_label] = tally.get(ith_label, 0) + 1
 
-    return sorted(tally.items(), key=operator.itemgetter(1), reverse=True)[0][0]
+    max_count = -1
+    best_label = ""
+    for label, count in tally.items():
+        if count > max_count:
+            best_label = label
+            max_count = count
+
+    return best_label
--- test.py
+++ test.py
@@ -5,9 +5,14 @@ import knn
 
 class TestClassify(unittest.TestCase):
     def test_knn_classify(self):
-        factors = numpy.array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
-        labels = ['A', 'A', 'B', 'B']
+        factors = numpy.array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1], [-1, -1], [-0.5, -1.1]])
+        labels = ['A', 'A', 'B', 'B', 'C', 'C']
+
         prediction = knn.classify([1.1, 0.9], factors, labels, 3)
         self.failUnless(prediction == 'A')
 
+        prediction = knn.classify([-0.1, 0.3], factors, labels, 3)
+        self.failUnless(prediction == 'B')
 
+        prediction = knn.classify([-0.7, -1.3], factors, labels, 3)
+        self.failUnless(prediction == 'C')
