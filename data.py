6e4c68a8382251f06a158657b7ee3c2f34e6ae69
Gary Yim <gary.k.yim@gmail.com>
2018-03-06 19:35:02 -0500

    Added testing for trees and entropy calculation.

--- data.py
+++ data.py
@@ -60,15 +60,6 @@ def normalize_cols(matrix):
     return normal_matrix, mins, ranges
 
 
-def create_bird_set():
-    matrix = [[1, 1, 'yes'],
-              [1, 1, 'yes'],
-              [1, 0, 'no'],
-              [0, 1, 'no'],
-              [0, 1, 'no']]
-    labels = ['no surfacing', 'flippers']
-    return matrix, labels
-
 
 
 
--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import unittest
 import numpy
 import knn
+import trees
+import math
 
 
 class TestClassify(unittest.TestCase):
@@ -16,3 +18,36 @@ class TestClassify(unittest.TestCase):
 
         prediction = knn.classify([-0.7, -1.3], factors, labels, 3)
         self.failUnless(prediction == 'C')
+
+    def test_calculate_entropy(self):
+        self.failUnless(trees.calculate_entropy([0]) == 0)
+        self.failUnless(trees.calculate_entropy([0, 0, 0, 1, 1, 1]) == -math.log(0.5, 2))
+        self.failUnless(trees.calculate_entropy(['A', 'B']) == -math.log(0.5, 2))
+        self.failUnless(trees.calculate_entropy(['A', 'B', 'C', 'D', 'E']) == -math.log(0.2, 2))
+        self.failUnless(trees.calculate_entropy([0, 0, 'A', 'A', 'A']) ==
+                        -0.4 * math.log(0.4, 2) +
+                        -0.6 * math.log(0.6, 2))
+
+    def test_trees_classify(self):
+        matrix = [[1, 1, 'yes'],
+                  [1, 1, 'yes'],
+                  [1, 0, 'no'],
+                  [0, 1, 'no'],
+                  [0, 1, 'no']]
+        labels = ['no surfacing', 'flippers']
+
+        tree = trees.create_tree(matrix, labels)
+
+        prediction = trees.classify([1, 1], tree, labels)
+        self.failUnless(prediction == 'yes')
+
+        prediction = trees.classify([1, 0], tree, labels)
+        self.failUnless(prediction == 'no')
+
+        prediction = trees.classify([0, 1], tree, labels)
+        self.failUnless(prediction == 'no')
+
+        prediction = trees.classify([0, 0], tree, labels)
+        self.failUnless(prediction == 'no')
+
+
